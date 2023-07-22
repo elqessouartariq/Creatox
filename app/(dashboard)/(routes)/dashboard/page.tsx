5db09e524d31b50adb2aef8729043c933d512a5c
elqessouartariq <tariqelqessouar@gmail.com>
2023-07-22 16:01:45 +0100

    feat: implement authentification

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,5 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
+
 const DashboardPage = () => {
-	return <p>Dash Page (protected)</p>;
+	return (
+		<>
+			<p>Dash Page (protected)</p>;
+			<UserButton afterSignOutUrl="/" />
+		</>
+	);
 };
 
 export default DashboardPage;
