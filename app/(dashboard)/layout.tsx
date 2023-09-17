da03fa749bcf5193bc07cdc0d349bf18c25a792d
elqessouartariq <tariqelqessouar@gmail.com>
2023-09-17 19:45:25 +0100

    refactor: removed z-index

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -11,7 +11,7 @@ export default async function DashboardLayout({
 
 	return (
 		<div className="h-full relative">
-			<div className=" hidden h-full md:w-72 md:flex md:flex-col  md:fixed md:inset-y-0 z-[80] bg-red-500">
+			<div className=" hidden h-full md:w-72 md:flex md:flex-col  md:fixed md:inset-y-0  bg-red-500">
 				<Sidebar apiLimitCount={apiLimitCount} />
 			</div>
 			<main className="md:pl-72">
